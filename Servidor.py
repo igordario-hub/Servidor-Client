f6747dabef51250f588006e4bea517c1af058890
igor.dario <55605189+igordario-hub@users.noreply.github.com>
2022-08-11 22:15:13 -0300

    Add files via upload

---
+++ Servidor.py
@@ -0,0 +1,26 @@
+from socket import socket, AF_INET, SOCK_DGRAM
+from threading import Thread
+
+HOST = 'localhost'
+PORT = 12708
+
+#criando Socket
+s_servidor = socket(AF_INET, SOCK_DGRAM)
+print('Socket criado com sucesso!')
+s_servidor.bind((HOST, 12708))
+s_servidor.listen()
+
+while True:
+    conn_socket, addr_client
+    print(f'O servidor aceitou a conexão do Cliente: {addr_client}')
+    Thread(target=HandleRequest, args=(conn_socket, addr_client)).start()
+
+def HandleRequest(conn_socket, addr_client):
+    while True:
+        print('Próxima mensagem')
+        data = s_client.recv(2048)
+        print(f'Cliente {addr_client}')
+        requi = data.decode()
+        requi= 'ok'
+        s_client.send(rep.encode())
+    
