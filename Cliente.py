bac0ba729b8a598ef71c239c97d1e06de8ddaaf2
igor.dario <55605189+igordario-hub@users.noreply.github.com>
2022-08-11 22:47:29 -0300

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,13 +1,16 @@
-from socket import socket, AF_INET, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_DGRAM
+HOST = 'localhost'
+PORT = 12708
 
 s_client = socket(AF_INET, SOCK_DGRAM)
-s_client.connect((HOST, 12708))
+s_client.connect((HOST, PORT))
 
 cont = int(input('Digite a quantidade de mensagem que você quer enviar:'))
-                 
+
 for i in rangue(cont):
     mensage = input('Digite uma mensagem para o servidor\n')
-    s_client.send(mensage.encode())
-    data = s_client.recv(2048)
+    s_client.sendto(mensage.encode())
+    data = s_client.recvfrom(2048)
     reply = data.decode()
     print(f'Servidor:{reply}')
+
